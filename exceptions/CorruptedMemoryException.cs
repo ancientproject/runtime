4e07edd09fb345d704082b4a2356c851ae27e27b
Yuuki Wesp <ls-micro@ya.ru>
2019-06-25 20:21:30 +0300

    flame -> Ancient

---
+++ exceptions/CorruptedMemoryException.cs
@@ -0,0 +1,10 @@
+﻿namespace flame.runtime.exceptions
+{
+    using System;
+
+    public class CorruptedMemoryException : Exception
+    {
+        public CorruptedMemoryException() { }
+        public CorruptedMemoryException(string msg) : base(msg) { }
+    }
+}
---
+++ exceptions/InvalidCharsException.cs
@@ -0,0 +1,9 @@
+﻿namespace flame.runtime.exceptions
+{
+    using System;
+
+    public class InvalidCharsException : Exception
+    {
+        public InvalidCharsException(string msg) : base(msg) { }
+    }
+}
