7fa862e0fe1111e7dd7219584ea2063f0b62beac
Yuuki Wesp <ls-micro@ya.ru>
2020-05-09 12:02:39 +0300

    add dif_t dif_f implementation

---
+++ src/instructions/dif_t.cs
@@ -0,0 +1,39 @@
+﻿namespace ancient.runtime
+{
+    using emit.@unsafe;
+
+    public class dif_t : Instruction
+    {
+        private readonly byte _cell;
+        private readonly byte _skip;
+
+        public dif_t(byte cell, byte skip) : base(IID.dif_t)
+        {
+            _cell = cell;
+            _skip = skip;
+        }
+        protected override void OnCompile()
+        {
+            var (r1, r2) = new d8u(_cell);
+            var (r3, u1) = new d8u(_skip);
+            base.Construct(r1, r2, r3, u1);
+        }
+    }
+    public class dif_f : Instruction
+    {
+        private readonly byte _cell;
+        private readonly byte _skip;
+
+        public dif_f(byte cell, byte skip) : base(IID.dif_f)
+        {
+            _cell = cell;
+            _skip = skip;
+        }
+        protected override void OnCompile()
+        {
+            var (r1, r2) = new d8u(_cell);
+            var (r3, u1) = new d8u(_skip);
+            base.Construct(r1, r2, r3, u1);
+        }
+    }
+}
