6551269abd3d54837040efe3cb591c15a84ff4ec
Yuuki Wesp <ls-micro@ya.ru>
2020-05-09 05:26:20 +0300

    add missed sin & sinh

---
+++ src/instructions/math/sin.cs
@@ -0,0 +1,8 @@
+﻿namespace ancient.runtime
+{
+    public class sin : InstructionDynamic
+    {
+        public sin(byte? resultCell, byte? cell1)
+            : base(IID.sin, resultCell, cell1) { }
+    }
+}
---
+++ src/instructions/math/sinh.cs
@@ -0,0 +1,8 @@
+﻿namespace ancient.runtime
+{
+    public class sinh : InstructionDynamic
+    {
+        public sinh(byte? resultCell, byte? cell1)
+            : base(IID.sinh, resultCell, cell1) { }
+    }
+}
