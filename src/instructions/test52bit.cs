2872d8bddb8224fde42bae8b08edd7f0c771a8de
Yuuki Wesp <ls-micro@ya.ru>
2020-05-18 05:19:55 +0300

    add test instructions

---
+++ src/instructions/test52bit.cs
@@ -0,0 +1,14 @@
+﻿namespace ancient.runtime
+{
+    public class test52bit : Instruction
+    {
+        public test52bit() : base(IID.reserved1)
+        {
+            
+        }
+        protected override void OnCompile()
+        {
+            Construct(1,2,3,4,5,6,7,8,9,10,11,12);
+        }
+    }
+}
