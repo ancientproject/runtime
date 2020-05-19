99d5615aa99eb73b1eb798945a60e4b761e58e31
Yuuki Wesp <ls-micro@ya.ru>
2020-05-19 06:28:02 +0300

    add return type in signature

--- src/instructions/sig.cs
+++ src/instructions/sig.cs
@@ -2,25 +2,27 @@
 {
     using System.Collections.Generic;
     using System.Text;
+    using emit.sys;
     using emit.@unsafe;
 
     public class sig : Instruction
     {
         private readonly string _name;
         private readonly byte _count;
-        private readonly int _returnTypeHash;
+        private readonly string _returnType;
 
-        public sig(string name, int count) : base(IID.sig)
+        public sig(string name, int count, string returnType) : base(IID.sig)
         {
             _name = name;
             _count = (byte)count;
+            _returnType = returnType;
         }
 
         protected override void OnCompile()
         {
             var (r1, r2) = new d8u(_count);
-            //var (r3, u1, u2, x1, x2, x3, n7, n8) = new d32u();
-            Construct(r1, r2);
+            var (r3, u1, u2, x1, x2, x3, x4, o1) = ExternType.FindAndDeconstruct(_returnType);
+            Construct(r1, r2, r3, u1, u2, x1, x2, x3, x4, o1);
         }
 
         public override bool HasMetadata() => true;
@@ -28,10 +30,10 @@
         {
             get
             {
-                var len = Encoding.UTF8.GetByteCount($"function->{_name}");
+                var len = Encoding.UTF8.GetByteCount($"function {_name}() -> {_returnType}");
                 var bu = new List<byte>();
                 bu.AddRange(MetaTemplate.By(TemplateType.STR).Len(len).ToBytes());
-                bu.AddRange(Encoding.UTF8.GetBytes($"function->{_name}"));
+                bu.AddRange(Encoding.UTF8.GetBytes($"function {_name}() -> {_returnType}"));
                 return bu.ToArray();
             }
         }
