ec4aa0cd877b858ca3cf8a1fd2cfaf0f29de0268
Yuuki Wesp <ls-micro@ya.ru>
2020-05-19 06:28:13 +0300

    refactoring finders

--- src/emit/sys/ExternType.cs
+++ src/emit/sys/ExternType.cs
@@ -21,25 +21,33 @@
 
         public static ulong FindAndConstruct(string code)
         {
-            const string target = "ancient.runtime.emit.sys.{0}_Type";
-            var path = string.Format(target, code);
-            var type = Type.GetType(path);
-            if(type is null)
-                throw new AggregateException($"Cannot find {path} type.");
+            ValidateTypeCode(code);
             return ulong.Parse($"00{string.Join("", Encoding.ASCII.GetBytes(code).Select(x => $"{x:X}"))}",
                 NumberStyles.AllowHexSpecifier);
+        }
+
+        public static d32i FindAndDeconstruct(string code)
+        {
+            ValidateTypeCode(code);
+            return new d32i(int.Parse($"{string.Join("", Encoding.ASCII.GetBytes(code).Select(x => $"{x:X}"))}"));
         }
 
         public static ExternType FindAndConstruct(ulong idx)
         {
             var code = string.Join("", Encoding.ASCII.GetString(BitConverter.GetBytes(idx))
                 .TrimEnd('\0').Reverse());
+            var type = ValidateTypeCode(code);
+            return Activator.CreateInstance(type) as ExternType;
+        }
+
+        private static Type ValidateTypeCode(string code)
+        {
             const string target = "ancient.runtime.emit.sys.{0}_Type";
             var path = string.Format(target, code);
             var type = Type.GetType(path);
             if(type is null)
                 throw new AggregateException($"Cannot find {path} type.");
-            return Activator.CreateInstance(type) as ExternType;
+            return type;
         }
 
         public string ShortName => this.GetType().Name.Replace("_Type", "");
