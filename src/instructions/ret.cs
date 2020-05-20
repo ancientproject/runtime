77a41f4cc80af2a47a44368f85c7b7f562500be3
Yuuki Wesp <ls-micro@ya.ru>
2020-05-20 11:22:05 +0300

    add return instruction

---
+++ src/instructions/ret.cs
@@ -0,0 +1,13 @@
+﻿namespace ancient.runtime
+{
+    public class ret : Instruction
+    {
+        public ret() : base(IID.ret)
+        {
+            
+        }
+        protected override void OnCompile()
+        {
+        }
+    }
+}
