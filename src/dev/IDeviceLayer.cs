fd3c8b92179ba04f2e80da3929aea4911b6e126e
Yuuki Wesp <ls-micro@ya.ru>
2020-06-05 00:26:59 +0300

    add support writing external devices

---
+++ src/dev/IDeviceLayer.cs
@@ -0,0 +1,17 @@
+﻿namespace ancient.runtime.devices
+{
+    using MagicOnion;
+
+    public interface IDeviceLayer : IService<IDeviceLayer>
+    {
+        UnaryResult<None> warmUp();
+        UnaryResult<None> shutdown();
+        UnaryResult<None> write(ulong address, ulong data);
+        UnaryResult<ulong> read(ulong address);
+    }
+
+    public struct None
+    {
+
+    }
+}
